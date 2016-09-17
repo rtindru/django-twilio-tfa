7d513a61231f0b772bc87dda08eb55d7f60d3429
Rajtilak Indrajit <indrajit@springboard.com>
2016-09-18 02:20:42 +0530

    Template touchups

--- tfa_demo/tfa_demo/settings.py
+++ tfa_demo/tfa_demo/settings.py
@@ -54,6 +54,11 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.contrib.messages.context_processors.messages',
+)
+
 ROOT_URLCONF = 'tfa_demo.urls'
 
 WSGI_APPLICATION = 'tfa_demo.wsgi.application'
